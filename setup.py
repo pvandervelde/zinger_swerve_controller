49e27d71348295bb56af43d14dc51667a11afa98
Petrik van der Velde <petrikvandervelde@gmail.com>
2023-08-24 23:13:46 +1200

    Ensure that we include the launch and config files in the install directory

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+import os
 from setuptools import setup
 
 package_name = 'zinger_swerve_controller'
@@ -7,9 +9,12 @@ setup(
     version='0.1.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        # Include all the config files
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
