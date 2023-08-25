81561d8b594989ff28987bc40ea236d1e9afd1a3
Petrik van der Velde <petrikvandervelde@gmail.com>
2023-08-25 23:00:03 +1200

    Thou shalt add the comma's in the right place

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         # Include all the config files
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml')))
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
