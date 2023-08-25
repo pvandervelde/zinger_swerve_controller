ff1cdacc346a3acd0fb146ec188643e50561a14e
Petrik van der Velde <petrikvandervelde@gmail.com>
2023-08-26 00:38:26 +1200

    Include the package entrypoint in the setup.py file

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'zinger_swerve_controller = zinger_swerve_controller.zinger_swerve_controller:main'
         ],
     },
 )
